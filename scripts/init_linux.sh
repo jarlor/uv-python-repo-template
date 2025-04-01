0d6eb30d2b508c4ecad429da892eb8ab173cae7a
jarlor <jarlorzhang@outlook.com>
2025-04-01 13:29:32 +0800

    refactor: adjust init_linux.sh

--- scripts/init_linux.sh
+++ scripts/init_linux.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 
-# 获取当前目录名
-dir_name=$(basename "$PWD")
+dir_name=$(basename "$PWD") # get the current directory name
+# replace - to _
+dir_name=$(echo "$dir_name" | sed 's/-/_/g')
 
-# 转义特殊字符以兼容sed
-escaped_dir_name=$(printf "%s" "$dir_name" | sed 's/[&/\]/\\&/g')
+escaped_dir_name=$(printf "%s" "$dir_name" | sed 's/[&/\]/\\&/g') # escape special characters for sed
 
-# 1. 替换pyproject.toml中的字符串
+# 1. Replace the string in pyproject.toml
 if [ -f "pyproject.toml" ]; then
-    sed -i.bak "s/python-repo-template/$escaped_dir_name/g" pyproject.toml && rm pyproject.toml.bak
+    sed -i.bak "s/uv_python_repo_template/$escaped_dir_name/g" pyproject.toml && rm pyproject.toml.bak
+    sed -i.bak "s/uv_python_repo_template/$escaped_dir_name/g" pyproject.toml && rm pyproject.toml.bak
+
 else
-    echo "错误：未找到 pyproject.toml 文件" >&2
+    echo "Error: pyproject.toml file not found" >&2
     exit 1
 fi
 
-# 2. 重命名目录
-if [ -d "python-repo-template" ]; then
-    mv "python-repo-template" "$dir_name"
+# 2. Rename the directory
+if [ -d "uv_python_repo_template" ]; then
+    mv "uv_python_repo_template" "$dir_name"
 else
-    echo "警告：未找到 'python-repo-template' 目录，跳过重命名" >&2
+    echo "Warning: 'uv_python_repo_template' directory not found, skipping renaming" >&2
 fi
 
-# 3. 重命名配置文件并执行命令
-if [ -f "pyproject.toml" ]; then
-    mv "pyproject.toml" "pyproject.toml.bak"
-    echo "已创建备份文件：pyproject.toml.bak"
-else
-    echo "错误：pyproject.toml 文件不存在，无法重命名" >&2
-    exit 1
-fi
-
-# 执行初始化命令
-uv run init
+# 3. Run the init command
+uv run poe init
